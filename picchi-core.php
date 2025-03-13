22d9bcec8c9c039fd4a5fc43014d19bde98d4c4d
AbirSupreoX <abir.supreox@gmail.com>
2025-03-13 17:59:01 +0600

    Picci Hero Widget Content tab Added

---
+++ picchi-core.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Plugin Name: Picchi Core
+ * Description: Picchi Core Addons for Picchi Theme.
+ * Version:     1.0.0
+ * Author:      Abir
+ * Author URI:  https://github.com/mdabir6789
+ * Text Domain: picchi-core
+ *
+ * Requires Plugins: elementor
+ * Elementor tested up to: 3.25.0
+ * Elementor Pro tested up to: 3.25.0
+ */
+
+function register_picchi_widgets($widgets_manager)
+{
+
+    require_once(__DIR__ . '/widgets/picchi-hero-widget.php');
+
+    $widgets_manager->register(new \Picchi_Hero_Widget());
+}
+add_action('elementor/widgets/register', 'register_picchi_widgets');
+
+
+function add_elementor_widget_categories($elements_manager)
+{
+    $elements_manager->add_category(
+        'picchi-category',
+        [
+            'title' => esc_html__('Picchi', 'picchi-core'),
+            'icon' => 'fa fa-plug',
+        ]
+    );
+}
+add_action('elementor/elements/categories_registered', 'add_elementor_widget_categories');
+
+
+
+// Registering Widget Styles
+function picchi_core_register_widget_styles()
+{
+    wp_register_style('picchi-core-style', plugins_url('assets/css/style.css', __FILE__));
+    wp_enqueue_style('picchi-core-style');
+}
+add_action('wp_enqueue_scripts', 'picchi_core_register_widget_styles');
---
+++ widgets/picchi-hero-widget.php
@@ -0,0 +1,200 @@
+<?php
+class Picchi_Hero_Widget extends \Elementor\Widget_Base
+{
+
+    public function get_name(): string
+    {
+        return 'picchi_hero';
+    }
+
+    public function get_title(): string
+    {
+        return esc_html__('Picchi Hero', 'picchi-core');
+    }
+
+    public function get_icon(): string
+    {
+        return 'eicon-code';
+    }
+
+    public function get_categories(): array
+    {
+        return ['picchi-category'];
+    }
+
+    public function get_keywords(): array
+    {
+        return ['picchi', 'hero'];
+    }
+
+    protected function register_controls(): void
+    {
+
+        // Content Tab Start
+
+        $this->start_controls_section(
+            'content',
+            [
+                'label' => esc_html__('Content', 'picchi_core'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+        //Subtitle
+        $this->add_control(
+            'sub_title',
+            [
+                'label' => esc_html__('Subtitle', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'placeholder' => esc_html__('Type your subtitle here', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+
+
+            ]
+        );
+        //Title
+        $this->add_control(
+            'title',
+            [
+                'label' => esc_html__('Title', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'placeholder' => esc_html__('Type your title here', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+
+
+            ]
+        );
+        //Description
+        $this->add_control(
+            'description',
+            [
+                'label' => esc_html__('Description', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::WYSIWYG,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'placeholder' => esc_html__('Type your description here', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+
+
+            ]
+        );
+
+        $this->end_controls_section();
+
+        // Content Tab End
+
+
+        // Content Tab Start
+
+        $this->start_controls_section(
+            'button',
+            [
+                'label' => esc_html__('Button', 'picchi_core'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+        //Button 1 text
+        $this->add_control(
+            'button1_text',
+            [
+                'label' => esc_html__('Button 1 Text', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'placeholder' => esc_html__('Type button text here', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+            ]
+        );
+        //Button 1 URL
+        $this->add_control(
+            'button1_url',
+            [
+                'label' => esc_html__('Button 1 URL', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::URL,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+            ]
+        );
+
+        //Button 2 text
+        $this->add_control(
+            'button2_text',
+            [
+                'label' => esc_html__('Button 2 Text', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'placeholder' => esc_html__('Type button text here', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+                'separator' => 'before'
+            ]
+        );
+        //Button 2 URL
+        $this->add_control(
+            'button2_url',
+            [
+                'label' => esc_html__('Button 2 URL', 'picchi_core'),
+                'type' => \Elementor\Controls_Manager::URL,
+                // 'default' => esc_html__('Default title', 'picchi_core'),
+                'label_block' => true,
+                'dynamic' => [
+                    'active' => true,
+                ],
+            ]
+        );
+        //Subheading Style
+
+
+    }
+
+    protected function render(): void
+    {
+        //Contents
+        $settings = $this->get_settings_for_display();
+        $sub_title = $settings['sub_title'];
+        $title = $settings['title'];
+        $description = $settings['description'];
+        //Buttons 1
+        $button1_text = $settings['button1_text'];
+        $button1_url = $settings['button1_url']['url'];
+        $this->add_link_attributes('link1_attribute', $settings['button1_url']);
+        //Buttons 2
+        $button2_text = $settings['button2_text'];
+        $button2_url = $settings['button2_url']['url'];
+        $this->add_link_attributes('link2_attribute', $settings['button2_url']);
+
+
+?>
+        <div class="welcome-content text-center">
+            <h4><?php echo $sub_title; ?></h4>
+            <h2><?php echo $title; ?></h2>
+            <p>
+                <?php echo $description; ?>
+            </p>
+            <a <?php $this->print_render_attribute_string('link1_attribute'); ?> class="box-btn">
+                <?php echo $button1_text ?></a>
+            <a <?php $this->print_render_attribute_string('link2_attribute'); ?>
+                class="border-btn"><?php echo $button2_text ?></a>
+
+        </div>
+<?php
+    }
+
+    protected function content_template(): void {}
+}
